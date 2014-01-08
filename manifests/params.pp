30c0874a6eb9fe36ce94792cdfb0f21d83cb8002
Vijay Tripathi <vijayt.124@gmail.com>
2014-01-07 18:38:35 -0800

    cleanup

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,13 @@
 class windows_python::params {
-  $python_source      = "${::temp}\\python.msi"
-  $python_remote      = 'http://www.python.org/ftp/python/2.7.3/python-2.7.3.msi'
-  $python_package     = 'Python 2.7.3'
-  $python_installdir  = 'C:\Python27'
-  $easyinstall_source = "${::temp}\\ez_setup.py"
-  $easyinstall_remote = 'https://bitbucket.org/pypa/setuptools/raw/0.8/ez_setup.py'
-  $pip_source         = "${::temp}\\get-pip.py"
-  $pip_remote         = 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py'
-  $chocolatey         = 'true'
+  $python_source             = "${::temp}\\python.msi"
+  $python_remote             = 'http://www.python.org/ftp/python/2.7.3/python-2.7.3.msi'
+  $python_package            = 'Python 2.7.3'
+  $python_installdir         = 'C:\Python27'
+  $easyinstall_source        = "${::temp}\\ez_setup.py"
+  $easyinstall_remote        = 'https://bitbucket.org/pypa/setuptools/raw/0.8/ez_setup.py'
+  $pip_source                = "${::temp}\\get-pip.py"
+  $pip_remote                = 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py'
+  $python_chocolatey         = 'true'
   $easyinstall_chocolatey    = false
 }
+
